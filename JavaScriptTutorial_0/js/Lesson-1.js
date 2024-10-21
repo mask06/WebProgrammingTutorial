42ac1918fd4b880adccf8867652e453f1da630ad
mask06 <Mustafaygar3903@gmail.com>
2024-10-21 12:56:14 +0300

    js giriş kodları

---
+++ JavaScriptTutorial_0/js/Lesson-1.js
@@ -0,0 +1,76 @@
+/* var metinselIfade = "Gitti";
+        var sayisalIfade = 5;
+        var mantiksalIfade = true;
+        var ojbectIfade = {};
+        var arrayIfade = [];
+        var arrayIfade2 = new Array();
+        var bosBirSey;
+ 
+        console.log(typeof(metinselIfade));
+        console.log(typeof(sayisalIfade));
+        console.log(typeof(mantiksalIfade));
+        console.log(typeof(ojbectIfade));
+        console.log(typeof(arrayIfade));
+        console.log(typeof(arrayIfade2));
+        console.log(typeof(bosBirSey));
+        console.log(typeof(yokBirSey));
+       
+        var mesaj = "Log Yazdım";
+        var mesaj = "test";
+        var sayi1 = 15;
+ 
+        {
+            console.log(sayi1);
+ 
+            //console.log(sayi); // Hata Uncaught ReferenceError
+ 
+            let sayi = 10;  
+ 
+            {
+                console.log(sayi); // Bu çalışacak
+ 
+                let sayi2 = 20;
+ 
+                console.log(sayi2); // Bu çalışacak
+            }
+ 
+            console.log(sayi); // Bu çalışacak
+            //console.log(sayi2); // Bu Hata            
+        }
+ 
+        console.log(sayi); // Hata sayi is not defined
+        console.log(sayi1);
+ 
+        function ClickMe()
+        {
+            //var mesaj = "Log Yazdım fonksiyon içinde";
+            //alert('Tıklandım.');
+            console.log(mesaj);
+            console.log(sayi);
+        } */
+
+//console.log(sayi);
+/* var adet = prompt("Kaç adet x istiyorsunuz?","0");
+
+for (let index = 1; index <= adet; index++) { 
+    
+    console.log(index+"- x değeriniz.");
+} */
+
+var adet = prompt("kaç adet t-shirt istiyorsunuz", "0");
+for (let index = 1; index <= adet; index++) {
+    var tshirt = prompt(index + " renk seç")
+
+    if (tshirt == "kirmizi") {
+        console.log("kırımızı");
+    }
+    else if (tshirt == "kirmizi") {
+        console.log("kırımızı");
+    }
+    else if (tshirt == "kirmizi") {
+        console.log("kırımızı");
+    }
+    else if (tshirt == "kirmizi") {
+        console.log("kırımızı");
+    }
+}
